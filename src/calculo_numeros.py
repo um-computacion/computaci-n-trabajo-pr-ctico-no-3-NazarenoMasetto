a16daf3d52c76bc7a6bf1f2155cc64bb45954d71
Nazareno Masetto <nazamasetto@gmail.com>
2025-04-15 20:21:53 -0300

    Implementación de excepciones y validación, manejo de excepciones

--- src/calculo_numeros.py
+++ src/calculo_numeros.py
@@ -1,4 +1,3 @@
-
 from src.exceptions import NumeroDebeSerPositivo
 
 def ingrese_numero():
@@ -7,3 +6,12 @@ def ingrese_numero():
     if numero < 0:
         raise NumeroDebeSerPositivo("El número debe ser positivo.")
     return numero
+
+if __name__ == '__main__':
+    try:
+        numero = ingrese_numero()
+        print(f"Número válido: {numero}")
+    except ValueError:
+        print("Error: La entrada debe ser un número válido.")
+    except NumeroDebeSerPositivo as e:
+        print(f"Error: {e}")
